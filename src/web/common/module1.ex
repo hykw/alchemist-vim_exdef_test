5de3ddf293991e8b382a1b360fe272207e9b9528
hykw <hykw1234@gmail.com>
2016-09-11 18:06:31 +0900

    found correct/wrong case

---
+++ src/web/common/module1.ex
@@ -0,0 +1,6 @@
+defmodule Sample.Module1 do
+  def foo do
+    "foo"
+  end
+
+end
--- src/web/controllers/page_controller.ex
+++ src/web/controllers/page_controller.ex
@@ -5,3 +5,28 @@ defmodule Sample.PageController do
     render conn, "index.html"
   end
 end
+
+
+defmodule Sample.WrongCase do
+  use Sample.Web, :controller
+
+  alias Sample.{
+    Module1,
+  }
+
+  def index(conn, _params) do
+    bar = Sample.Module1.foo
+
+    render conn, "index.html"
+  end
+end
+
+defmodule Sample.CorrectCase do
+  use Sample.Web, :controller
+
+  def index(conn, _params) do
+    bar = Sample.Module1.foo
+
+    render conn, "index.html"
+  end
+end
